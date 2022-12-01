11b4fb92f5e95feb0e0b16b9bafa9ada2cd24206
MYZ <17602951546@163.com>
2022-12-01 21:35:23 +0800

    Update jd_red_package.js

--- jd_red_package.js
+++ jd_red_package.js
@@ -1,3 +1,3 @@
 var jd = JSON.parse($response.body);
-(jd.data.result.supportCashOut=1,jd.data.result.canExchangeRedpack=0,jd.data.result.withdrawList[0].status=1,jd.data.result.withdrawList[1].status=1,jd.data.result.withdrawList[2].status=0), $done({
+(jd.data.result.supportCashOut=1,jd.data.result.canExchangeRedpack=0,jd.data.result.withdrawList[0].status=0,jd.data.result.withdrawList[1].status=0,jd.data.result.withdrawList[2].status=0), $done({
 	body: JSON.stringify(jd)});
