00f315a4a6635445720faf92ec2a8a82e77f2113
MYZ <17602951546@163.com>
2022-12-01 21:37:30 +0800

    Update jd_red_package.js

--- jd_red_package.js
+++ jd_red_package.js
@@ -1,3 +1,4 @@
 var jd = JSON.parse($response.body);
 (jd.data.result.supportCashOut=1,jd.data.result.canExchangeRedpack=0,jd.data.result.withdrawList[0].status=0,jd.data.result.withdrawList[1].status=0,jd.data.result.withdrawList[2].status=0), $done({
 	body: JSON.stringify(jd)});
+ 
