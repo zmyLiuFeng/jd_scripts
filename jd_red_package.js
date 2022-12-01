bb6c1063e2d4f47a33dcce96d6609cf14662e626
MYZ <17602951546@163.com>
2022-12-01 21:11:37 +0800

    Update jd_red_package.js

--- jd_red_package.js
+++ jd_red_package.js
@@ -1,3 +1,3 @@
 var jd = JSON.parse($response.body);
 (jd.withdrawList[0].status=0,jd.withdrawList[1].status=0,jd.withdrawList[2].status=0), $done({
-	body: JSON.stringify(ddgksf2013)
+	body: JSON.stringify(jd)
